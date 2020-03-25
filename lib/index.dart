0e1840d5bf47a91fb0735587b82036d47750d732
Taufiq Dwi Cahyono <taufiqdwic@gmail.com>
2020-03-25 15:11:22 +0700

    add index

---
+++ lib/index.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tracking_corona/provider/corona_provider.dart';
+
+class IndexApp extends StatelessWidget {
+  const IndexApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => CoronaProvider(),
+        )
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Kawal Corona - Taufiq Jack',
+      ),
+    );
+  }
+}
